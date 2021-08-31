1ee2ed0d7647f3ffc23a1ac129d1b868b43522f2
Manuel Alejandro Moscoso Dominguez <manuel@mobiquos.cl>
2021-08-31 09:19:41 -0400

    Primer commit de la rama develop con la primera version del landing page de construccion

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
     echo '<link href="css/entrenamiento.css" type="text/css" rel="stylesheet" media="screen,projection"/>';
-    echo "<h1>Hola Mundo, Entranamiento App</h1>";
+    echo "<h1>En Construcción: Entranamiento App</h1>";
 ?>
